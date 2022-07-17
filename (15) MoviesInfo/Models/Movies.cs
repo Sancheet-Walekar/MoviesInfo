f6d045ecda857d8e59c26b8933ea8a5d583dd775
18sancheet <sancheetwalekar@gmail.com>
2022-07-17 21:24:24 +0530

    first commit

---
+++ (15) MoviesInfo/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesInfo.Models
+{
+    public class Movies
+    {
+        public virtual int MoviesId { get; set; }
+        public virtual String MoviesName { get; set; }
+        public virtual String MoviesCategory { get; set; }
+        public virtual float MoviesRatings { get; set; }
+    }
+}
